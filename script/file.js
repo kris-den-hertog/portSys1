1c6779614da255f9cc565031893df90d9b5ea52b
Your Name <you@example.com>
2023-09-20 11:52:33 +0200

    frenchcore added

---
+++ script/file.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+const french = document.getElementById("fc");
+
+french.addEventListener("click", function () {
+    window.open("https://github.com/kris-den-hertog/frenchcore---fixed--", "_blank");
+
+})
+
+
+
+});
