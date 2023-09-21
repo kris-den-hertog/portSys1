da58ae96609c4c23645d68f304c4be4819a11da1
Your Name <you@example.com>
2023-09-21 11:58:07 +0200

    test

--- script/file.js
+++ script/file.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
 const french = document.getElementById("fc");
 const about = document.getElementById("am");
+const port = document.getElementById("pf");
 
-alert("Thank you for testing the BETA of my portfolio. If you have any feedback on this site, feel free to let me know via my social media accounts!");
+//alert("Thank you for testing the BETA of my portfolio. If you have any feedback on this site, feel free to let me know via my social media accounts!");
 french.addEventListener("click", function () {
+    window.open("https://github.com/kris-den-hertog/frenchcore---fixed--", "_blank");
+
+})
+port.addEventListener("click", function () {
     window.open("https://github.com/kris-den-hertog/frenchcore---fixed--", "_blank");
 
 })
